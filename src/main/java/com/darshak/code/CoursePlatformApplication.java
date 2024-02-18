5de95b81b82e973f11d6e087c088504179d4201e
Darshak Kachchhi <87572369+darshak-k@users.noreply.github.com>
2024-02-18 16:27:17 -0500

    Initial Code

---
+++ src/main/java/com/darshak/code/CoursePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.darshak.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursePlatformApplication.class, args);
+	}
+
+}
